731778ff89d81064af2eb4c15a26ddf812a2779d
StrategFBI <tapeshko1@rambler.ru>
2023-09-23 10:17:31 +0300

    client

---
+++ components/modules/Header/ProfileDropdown.tsx
@@ -0,0 +1,59 @@
+import ProfileSvg from "@/components/elements/ProfileSvg/ProfileSvg";
+import { $mode } from "@/context/mode";
+import { IWrappedComponentProps } from "@/types/common";
+import { useStore } from "effector-react";
+import { forwardRef } from "react";
+import styles from '@/styles/profileDropdown/index.module.scss'
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+import LogoutSvg from "@/components/elements/LogoutSvg/Logout";
+import { withClickOutside } from "@/utils/withClickOutside";
+
+
+const ProfileDropdown = forwardRef<HTMLDivElement, IWrappedComponentProps>(
+    ({open, setOpen}, ref) => {
+        const mode = useStore($mode)
+        const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+        const toggleProfileDropdown = () => setOpen(!open)
+        return (
+          <div className={styles.profile}ref={ref}>
+            <button className={styles.profile__btn} onClick={toggleProfileDropdown}>
+              <span className={styles.profile__span}>
+                <ProfileSvg />
+              </span>
+            </button>
+            <AnimatePresence>
+              {open && (
+                <motion.ul
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  className={`${styles.profile__dropdown} ${darkModeClass}`}
+                  style={{ transformOrigin: 'right top' }}
+                >
+                  <li className={styles.profile__dropdown__user}>
+                    <span className={`${styles.profile__dropdown__username} ${darkModeClass}`}>
+                      Ivan
+                    </span>
+                    <span className={`${styles.profile__dropdown__email} ${darkModeClass}`}>
+                      ivan@gmail.com
+                    </span>
+                  </li>
+                  <li className={styles.profile__dropdown__item}>
+                    <button className={styles.profile__dropdown__item__btn}>
+                      <span className={`${styles.profile__dropdown__item__text} ${darkModeClass}`}>Выйти</span>
+                      <span className={`${styles.profile__dropdown__item__svg} ${darkModeClass}`}>
+                        <LogoutSvg />
+                      </span>
+                    </button>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </div>
+        )
+    }
+)
+
+ProfileDropdown.displayName = 'ProfileDropdown'
+export default withClickOutside(ProfileDropdown)
